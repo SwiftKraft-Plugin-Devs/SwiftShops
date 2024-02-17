6954311e58168f9661fc9a2eec3e82b5c38b0632
SwiftKraft <76916776+SwiftKraft@users.noreply.github.com>
2024-02-17 14:37:02 -0500

    afdsdfasdafsdfas

--- EventHandler.cs
+++ EventHandler.cs
@@ -1,6 +1,17 @@
-﻿namespace SwiftShops
+﻿using PluginAPI.Core.Attributes;
+using PluginAPI.Enums;
+using PluginAPI.Events;
+using SwiftShops.API;
+
+namespace SwiftShops
 {
     public class EventHandler
     {
+        [PluginEvent(ServerEventType.PlayerLeft)]
+        public void PlayerLeft(PlayerLeftEvent _event)
+        {
+            if (ShopManager.Balance.ContainsKey(_event.Player))
+                ShopManager.Balance.Remove(_event.Player);
+        }
     }
 }
